17655e489d0e56f4ca06765c85e65393a1d4c1bd
paranoikmen <bogomolov222@gmail.com>
2021-06-30 20:01:20 +0300

    add firebase connect to index.js add main functional on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import React, {useState} from "react";
+import {useCollectionData} from "react-firebase-hooks/firestore"
+import firebase from "firebase";
 
-import './App.css';
+/**
+ * Component for showing chat message and send message
+ *
+ * @component App
+ */
 
-function App() {
-  return (
-    <div className="App">
+function App({firestore}) {
+    const [text, setText] = useState('')
+    const [messages, loading] = useCollectionData(
+        firestore.collection('messages').orderBy('createdAt')
+    )
 
-    </div>
-  );
+    const handleMessage = () => {
+        firestore.collection('messages').add({
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            text: text
+        })
+        setText('')
+    }
+
+    if (loading) {
+        return <div>Загрузка...</div>
+    }
+
+
+    return (
+        <div style={{textAlign: "center", marginTop: "50px"}}>
+            <div>
+                <input type="text" placeholder={"text message..."} onChange={event => setText(event.target.value)} value={text}/>
+                <button onClick={handleMessage}>Send</button>
+            </div>
+            <ul>
+                {
+                    messages.map(message =>
+                        <li style={{listStyleType: "none", fontSize: "2.5rem", marginTop: "40px"}}>
+                            {message.text}
+                        </li>)
+                }
+            </ul>
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from "firebase";
+import 'firebase/firestore'
 
+firebase.initializeApp({
+        apiKey: "AIzaSyAWl13-VFufa-3_R2ulomkcaEHp6wws-9w",
+        authDomain: "womenupchat.firebaseapp.com",
+        projectId: "womenupchat",
+        storageBucket: "womenupchat.appspot.com",
+        messagingSenderId: "638732852103",
+        appId: "1:638732852103:web:48b90c533783363b5e9122"
+    }
+);
+
+const firestore = firebase.firestore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App firestore={firestore}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
